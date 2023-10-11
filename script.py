325756a9f4d00f12bf605664fe750ad187529abc
jslyonnais <jslyonnais@devjs.io>
2023-10-11 17:02:09 -0400

    Revert "restructure code"
    This reverts commit ee874db66ba9b3f35e07eb10f794e5c603f57999.

--- script.py
+++ script.py
@@ -9,17 +9,16 @@ import time
 import argparse
 
 # Argument parsing setup
-def parse_args():
-    parser = argparse.ArgumentParser(description="Crawl Etsy shop items.")
-    parser.add_argument("--debug", action="store_true", help="Save debug HTML files for each item.")
-    parser.add_argument("--url", type=str, help="Etsy shop URL to crawl. If not provided, the script will prompt for it.")
-    return parser.parse_args()
+parser = argparse.ArgumentParser(description="Crawl Etsy shop items.")
+parser.add_argument("--debug", action="store_true", help="Save debug HTML files for each item.")
+parser.add_argument("--url", type=str, help="Etsy shop URL to crawl. If not provided, the script will prompt for it.")
+args = parser.parse_args()
 
 # Function to display spinner animation in terminal
-def spinner_animation(run_spinner):
+def spinner_animation():
     spinner = ['-', '\\', '|', '/']
     i = 0
-    while run_spinner():
+    while run_spinner:
         sys.stdout.write(spinner[i] + ' Processing... \r')
         sys.stdout.flush()
         time.sleep(0.2)
@@ -30,6 +29,7 @@ def get_item_details(url, debug=False):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
 
+     # If debug mode is active, save the soup object to an HTML file
     if debug:
         debug_dir = "debug"
         if not os.path.exists(debug_dir):
@@ -43,6 +43,7 @@ def get_item_details(url, debug=False):
 
     name = soup.find('h1').text.strip()
 
+    # Extracting price from the provided structure
     price_div = soup.find('div', {'data-buy-box-region': 'price'})
     if price_div:
         price_tag = price_div.find('p')
@@ -50,6 +51,7 @@ def get_item_details(url, debug=False):
     else:
         price = 'N/A'
 
+    # Extract the numeric review count from within the <span> tag of the reviews object
     reviews = soup.find('button', {'id': 'same-listing-reviews-tab'})
     if reviews:
         review_span = reviews.find('span')
@@ -59,57 +61,48 @@ def get_item_details(url, debug=False):
 
     return name, price, review_count, url
 
-# Main function
-def main(args):
-    # Set up spinner thread
-    run_spinner_flag = True
-    run_spinner = lambda: run_spinner_flag  # To control spinner from outside the thread
-    spinner_thread = threading.Thread(target=spinner_animation, args=(run_spinner,))
-    spinner_thread.daemon = True
-    spinner_thread.start()
+# Fetch the main page content
+if args.url:
+    url = args.url
+else:
+    url = input("Please enter the Etsy shop URL to crawl: ")
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
 
-    # Get URL to crawl
-    if args.url:
-        url = args.url
-    else:
-        url = input("Please enter the Etsy shop URL to crawl: ")
+# Start spinner animation in a separate thread
+run_spinner = True
+spinner_thread = threading.Thread(target=spinner_animation)
+spinner_thread.daemon = True
+spinner_thread.start()
 
-    # Get HTML content from URL
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
+# Extract shop name
+shop_name = soup.find('h1').text.strip()
 
-    # Get shop name
-    shop_name = soup.find('h1').text.strip()
+# Find all item URLs
+item_urls = [link['href'] for item in soup.find_all('div', {'data-appears-component-name': 'shop_home_listing_grid'}) for link in item.find_all('a') if link.has_attr('href')]
 
-    # Get URLs of all items in the shop
-    item_urls = [link['href'] for item in soup.find_all('div', {'data-appears-component-name': 'shop_home_listing_grid'}) for link in item.find_all('a') if link.has_attr('href')]
+# Fetch details for each item
+item_details = [get_item_details(item_url, debug=args.debug) for item_url in item_urls]
 
-    # Get details of each item
-    item_details = [get_item_details(item_url, debug=args.debug) for item_url in item_urls]
+# Create a DataFrame
+df = pd.DataFrame(item_details, columns=['Name', 'Price', 'Reviews', 'URL'])
 
-    # Create a DataFrame from the item details
-    df = pd.DataFrame(item_details, columns=['Name', 'Price', 'Reviews', 'URL'])
+# Check if output directory exists, if not create it
+output_dir = "output"
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
 
-    # Create output directory if it doesn't exist
-    output_dir = "output"
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
+# Create a unique filename with shop name and timestamp
+timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+filename = f"{shop_name}_{timestamp}.csv"
+filepath = os.path.join(output_dir, filename)
 
-    # Save DataFrame to CSV file
-    timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
-    filename = f"{shop_name}_{timestamp}.csv"
-    filepath = os.path.join(output_dir, filename)
-    df.to_csv(filepath, index=False)
+# Save the DataFrame to a CSV file
+df.to_csv(filepath, index=False)
 
-    # Stop spinner animation
-    run_spinner_flag = False
-    time.sleep(0.5)
-    spinner_thread.join()
+# Once scraping is done, stop the spinner
+run_spinner = False
+time.sleep(0.5)
+spinner_thread.join()
 
-    # Return CSV data as string
-    return df.to_csv(index=False)
-
-if __name__ == "__main__":
-    args = parse_args()
-    csv_string = main(args)
-    print("Processing complete!")
+print("Processing complete!")
