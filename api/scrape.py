509c1e1fd2b73262d097d6d6c145dc4b3657a1da
jslyonnais <jslyonnais@devjs.io>
2023-10-12 09:29:22 -0400

    update code deploy

--- api/scrape.py
+++ api/scrape.py
@@ -2,6 +2,11 @@ from http.server import BaseHTTPRequestHandler
 import json
 from script import main
 
+class Args:
+    def __init__(self, url, debug=False):
+        self.url = url
+        self.debug = debug
+
 class handler(BaseHTTPRequestHandler):
 
     def do_POST(self):
@@ -10,9 +15,10 @@ class handler(BaseHTTPRequestHandler):
         data = json.loads(post_data)
 
         url = data.get('url')
-
+        
         try:
-            csv_data = main(url)
+            args = Args(url)
+            csv_data = main(args)
             response = {
                 'csv': csv_data,
                 'filename': 'output.csv'
